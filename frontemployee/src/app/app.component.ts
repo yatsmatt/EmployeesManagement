c1a21e9ec6a49f05d8d00835f5a513f675142467
yatsilmatt <yatsilmatt@gmail.com>
2024-04-06 13:35:09 +0300

    basic ui pattern

--- frontemployee/src/app/app.component.ts
+++ frontemployee/src/app/app.component.ts
@@ -2,21 +2,24 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
 export class AppComponent implements OnInit{
-  employees:Employee[] | undefined ;
+  employees:Employee[] | undefined;
 
   constructor(private employeeService: EmployeeService){}
 
   ngOnInit() {
     this.getEmployees();
   }
+  
+
+
 
   public getEmployees(): void {
     this.employeeService.getEmployees().subscribe(
